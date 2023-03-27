2f77c8c6431845e27a555eed34a0922cb815413a
ananos548 <inabsm@mail.ru>
2023-03-27 21:44:05 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+
+from base.schemas import *
+
+app = FastAPI()
+
+
+@app.get('/users/', response_model=list[User])
+async def get_user(user: User):
+    return user
+
+
+@app.post('/tasks/')
+async def create_task(task: Task):
+    return task
