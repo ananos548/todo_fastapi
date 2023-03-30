0f63f3fd1b9809d15bf8c7cd393ca4fd365ec011
ananos548 <inabsm@mail.ru>
2023-03-31 00:17:05 +0300

    change structure

---
+++ src/main.py
@@ -0,0 +1,35 @@
+import uuid
+from fastapi import FastAPI, Depends
+from fastapi_users import FastAPIUsers
+
+from auth.base_config import auth_backend
+
+from auth.manager import get_user_manager
+from auth.schemas import UserRead, UserCreate
+from auth.models import User
+
+app = FastAPI()
+
+fastapi_users = FastAPIUsers[User, uuid.UUID](
+    get_user_manager,
+    [auth_backend],
+)
+
+app.include_router(
+    fastapi_users.get_auth_router(auth_backend),
+    prefix="/auth/jwt",
+    tags=["auth"],
+)
+
+app.include_router(
+    fastapi_users.get_register_router(UserRead, UserCreate),
+    prefix="/auth",
+    tags=["auth"],
+)
+
+current_user = fastapi_users.current_user()
+
+
+@app.get("/protected-route")
+def protected_route(user: User = Depends(current_user)):
+    return f"Hello, {user.username}"
